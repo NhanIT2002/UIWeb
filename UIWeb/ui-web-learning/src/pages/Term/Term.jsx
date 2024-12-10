3bab93c57f225416054476d42bb639755183154c
NhanIT2002 <nhannlt2002@gmail.com>
2024-12-10 12:14:36 +0700

    create page term of service

---
+++ UIWeb/ui-web-learning/src/pages/Term/Term.jsx
@@ -0,0 +1,167 @@
+import React from "react";
+
+import "./Term.css";
+
+const Term = () => {
+    return (
+        <div className="term" id="term">
+            <div className="container">
+                <div className="title-content">
+                    <h1>Term of Service</h1>
+                    <p className="content-des">Thank you for using WaniKani! We’re happy you’re here. Please read this Terms of Service Agreement carefully before accessing or using WaniKani. Because it is such an important contract between us and our users, we have tried to make it as clear as possible. For your convenience, we have presented these terms in a short non-binding summary followed by the full legal terms.</p>
+                    <hr />
+                </div>
+                <div className="summary">
+                    <h2>Summary</h2>
+                    <table className="summary-table">
+                        <thead>
+                            <tr>
+                                <th>Section</th>
+                                <th>What can you find here?</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <a href="#">A. Definitions</a>
+                                </td>
+                                <td>Some basic terms, defined in a way that will help you understand this Agreement. Refer back up to this section for clarification.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">B. Account Terms</a>
+                                </td>
+                                <td>These are the basic requirements of having an account on WaniKani.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">C. Acceptable Use</a>
+                                </td>
+                                <td>These are the basic rules you must follow when using your WaniKani account.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">D. User-Generated Content</a>
+                                </td>
+                                <td>You own the content you post on WaniKani. However, you have some responsibilities regarding it, and we ask you to grant us some rights so we can provide Services to you.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">E. Copyright & DMCA Policy</a>
+                                </td>
+                                <td>This section talks about how WaniKani will respond if you believe someone is infringing your copyrights on WaniKani.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">F. Intellectual Property Notice</a>
+                                </td>
+                                <td>This describes WaniKani’s rights in the Website and Service.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">G. API Terms</a>
+                                </td>
+                                <td>These are the rules for using WaniKani’s APIs, whether you are using the API for development or data collection.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">H. Advertising on WaniKani</a>
+                                </td>
+                                <td>If you want to promote your content on WaniKani, this section lays out some rules you must follow.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">I. Payment</a>
+                                </td>
+                                <td>You are responsible for payment. We are responsible for billing you accurately.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">J. Cancellation and Termination</a>
+                                </td>
+                                <td>You may cancel this Agreement and close your account at any time</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">K. Communications with Tofugu</a>
+                                </td>
+                                <td>We only use email and other electronic means to stay in touch with our users. We do not provide phone support.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">L. Disclaimer of Warranties</a>
+                                </td>
+                                <td>We provide our Service as is, and we make no promises or guarantees about this Service. <strong>Please read this section carefully; you should understand what to expect</strong>.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">M. Limitation of Liability</a>
+                                </td>
+                                <td>We will not be liable for damages or losses arising from your use or inability to use the Service or otherwise arising under this Agreement. <strong>Please read this section carefully; it limits ours obligations to you</strong>.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">N. Release and Indemnification</a>
+                                </td>
+                                <td>You are fully responsible for your use of the Service.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">O. Changes to These Terms</a>
+                                </td>
+                                <td>We may modify this Agreement, but we will give you 30 days notice of changes that affect your rights.</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">P. Miscellaneous</a>
+                                </td>
+                                <td>Please see this section fo legal details including our choice of law.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <hr />
+                </div>
+                <div className="content-details">
+                    <div className="title-detail">
+                        <h2>The WaniKani Term of Service</h2>
+                        <p>Effective date: May 25, 2023</p>
+                    </div>
+                    <div className="definition" id="definition">
+                        <h3>A. Definitions</h3>
+                        <hr />
+                        <div className="des">
+                            <p ><strong>Short version: </strong>We use these basic terms throughout the Agreement, and they have specific meanings. You should know what we mean when we use each of the terms. There’s not going to be a test on it, but it’s still useful information.</p>
+                        </div>
+                        <div className="content">
+                            <ul>
+                                <li>“Agreement” refers, collectively, to all the terms, conditions, notices contained or referenced in this document (the “Terms of Service” or the “Terms”) and all other operating rules, policies (including the WaniKani Privacy Policy, available at <a href="">wanikani.com/privacy</a>) and procedures that we may publish from time to time on the Website.</li>
+                                <li>“Content” refers to content featured or displayed through the Website, including without limitation text, data, articles, images, photographs, graphics, software, applications, designs, features, audio, and other materials that are available on the Website or otherwise available through the Service. “Content” also includes Services. “User-Generated Content” is Content, written or otherwise, created or uploaded by our Users. “Your Content” is Content that you create or own.</li>
+                                <li>“Service” refers to the applications, software, products, and services provided by Tofugu.</li>
+                                <li>“Tofugu,” “We,” and “Us” refer to Tofugu, LLC., as well as our affiliates, directors, subsidiaries, contractors, licensors, officers, agents, and employees.</li>
+                                <li>“User,” “You,” and “Your” refer to the individual person, company, or organization that has visited or is using the Website or Service; that accesses or uses any part of the account; or that directs the use of the account in the performance of its functions. A User must be at least 13 years of age. Special terms may apply for business or government accounts (See <a href="">Section B.4): Additional Terms</a>).</li>
+                                <li>“Website” or “WaniKani” refers to Tofugu’s website located at <a href="https://www.wanikani.com/">wanikani.com</a>, and all content, Services, and products provided by Tofugu at or through the Website. It also refers to Tofugu-owned subdomains of wanikani.com, such as <a href="https://api.wanikani.com/">api.wanikani.com</a>. Occasionally, Websites owned by Tofugu may provide different or additional terms of service. If those additional terms conflict with this Agreement, the more specific terms apply to the relevant page or Service.
+                                B. Account Terms</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="account-term" id="account-term">
+                        <h3>B. Account Terms</h3>
+                        <hr />
+                        <div className="des">
+                            <p ><strong>Short version: </strong>A human must create your account; you must be 13 or over; and you must provide a valid email address. You alone are responsible for your account and anything that happens while you are signed in to or using your account. You are responsible for keeping your account secure.</p>
+                        </div>
+                        <div className="content">
+                            <ul>
+                                <strong><li>Required Information</li></strong>
+                                <p>You must provide a valid email address in order to complete the signup process. Any other information requested, such as your real name, is optional, unless you are accepting these terms on behalf of a legal entity (in which case we need more information about the legal entity) or if you opt for a paid account, in which case additional information will be necessary for billing purposes.</p>
+                                <strong><li>Account Requirements</li></strong>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Term;
